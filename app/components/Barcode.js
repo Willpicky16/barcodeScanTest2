1f76429363f831f7e75a460777bba32923339df5
William Pickering <willpicky@gmail.com>
2017-04-20 16:39:09 +0100

    Barcode section now same as old version app

--- app/components/Barcode.js
+++ app/components/Barcode.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { StyleSheet, Dimensions, Text, View } from 'react-native';
 import Camera from 'react-native-camera';
+import axios from 'axios';
 
 const ROOT = 'https://gentle-escarpment-88131.herokuapp.com/api/products';
 
 export default class Barcode extends Component {
   scanBarcode(data) {
-    alert(data);
+    getName(data.data);
     }
   render () {
     return (
@@ -18,6 +19,29 @@ export default class Barcode extends Component {
       </Camera>
     )
   }
+}
+
+function getName (EAN) {
+  axios
+      .get(`${ROOT}/${EAN}`)
+      .then(function scanBarcode (data) {
+        alert(`${data.data.products[0].name} (${EAN}) Packaging: ${data.data.products[0].packaging}`);
+      })
+      .catch(function (error) {
+        // alert('Hello')
+        postProduct(EAN);
+    });
+}
+
+function postProduct (EAN) {
+  axios
+    .post(ROOT, {
+      code: EAN,
+      name: 'Test',
+      packaging: 'Test Packaging'
+    })
+    .then((data) => {alert(`Product ${EAN} added as Test`)})
+    .catch((err) => {alert(`Product ${EAN} already in Database - Please scan again!`)});
 }
 
 const styles = StyleSheet.create({
